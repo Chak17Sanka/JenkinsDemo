fc5610671dc51d3616e3681990a0b9ee8a5232eb
Chakradhar <chakradhar17.sanka@gmail.com>
2021-07-07 14:19:05 +0530

    Initial Commit for Jenkins Demo Project

---
+++ src/main/java/org/local/jenkins/jenkins_demo/App.java
@@ -0,0 +1,13 @@
+package org.local.jenkins.jenkins_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Welcome to a Quick Jenkins Demo Project" );
+    }
+}
