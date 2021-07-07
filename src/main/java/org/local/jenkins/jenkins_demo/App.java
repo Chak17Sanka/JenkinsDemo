c34ad9cfc08792e4df18cf1ae94b3d41f3a10ed6
Chakradhar <chakradhar17.sanka@gmail.com>
2021-07-07 19:07:06 +0530

    Added a variable in the program

--- src/main/java/org/local/jenkins/jenkins_demo/App.java
+++ src/main/java/org/local/jenkins/jenkins_demo/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+    	int a=0;
         System.out.println( "Hello World! Welcome to a Quick Jenkins Demo Project, This contains all the basic information required" );
     }
 }
