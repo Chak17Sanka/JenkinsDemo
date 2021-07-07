09dc69f0a6f6a34edd4a1c1c24399a5aeae60da6
Chakradhar <chakradhar17.sanka@gmail.com>
2021-07-07 17:51:40 +0530

    added new line

--- src/main/java/org/local/jenkins/jenkins_demo/App.java
+++ src/main/java/org/local/jenkins/jenkins_demo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Welcome to a Quick Jenkins Demo Project !!!!" );
+        System.out.println( "Hello World! Welcome to a Quick Jenkins Demo Project, This contains all the basic information required" );
     }
 }
