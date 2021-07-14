95c6922f6a4824ef7a15bfbbf8f3ea2745b86b78
Chakradhar <chakradhar17.sanka@gmail.com>
2021-07-15 01:30:11 +0530

    import the sqlserver.jdbc class

--- src/main/java/org/local/jenkins/jenkins_demo/App.java
+++ src/main/java/org/local/jenkins/jenkins_demo/App.java
@@ -9,6 +9,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
+import com.microsoft.sqlserver.jdbc.SQLServerDriver;
 
 /**
  * Hello world!
